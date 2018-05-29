6f6440ffe29c8897fc3e081994ae9467387936ba
lucaspasper <paschoalucas@gmail.com>
2018-05-29 11:07:32 -0300

    Aula Java 1

---
+++ javaFunction/src/javafunction/javaOperacoes.java
@@ -0,0 +1,22 @@
+/*
+ * To change this license header, choose License Headers in Project Properties.
+ * To change this template file, choose Tools | Templates
+ * and open the template in the editor.
+ */
+package javafunction;
+
+/**
+ *
+ * @author lpaschoal
+ */
+public class javaOperacoes {
+
+    public static String contador(int i, int f){
+        String s = "";
+        for(int x = i; x <= f; x++){
+            s += x + " ";
+        }
+        return s;
+    }
+    
+}
